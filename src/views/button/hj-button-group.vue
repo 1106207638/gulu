c99282e3a39ec50ebc685275c8a8de0537ceb415
qinxuyang <1106207638@qq.com>
2022-07-06 23:09:55 +0800

    hj-button-group新增检测子元素是否为hj-button的判断

--- src/views/button/hj-button-group.vue
+++ src/views/button/hj-button-group.vue
@@ -4,7 +4,19 @@
   </div>
 </template>
 <script lang="ts">
-export default {};
+export default {
+  mounted() {
+    console.log(this.$el.children)
+    for(let i=0;i<this.$el.children.length;i++){
+        let oi = this.$el.children[i]
+        let name = oi.nodeName.toLowerCase()
+        console.log(name)
+        if(name !=='button'){
+            console.warn(`子元素存在非button标签，请将${name}标签换为hj-button标签`)
+        }
+    }
+  },
+};
 </script>
 <style lang="less" scoped>
 .hj-button-group {
@@ -20,9 +32,9 @@ export default {};
       border-top-right-radius: var(--border-radius);
       border-bottom-right-radius: var(--border-radius);
     }
-    &:hover{
-        position: relative;
-        z-index: 1;
+    &:hover {
+      position: relative;
+      z-index: 1;
     }
   }
 }
