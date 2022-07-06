c65445439e95002a195c6c4a89d31760ab207490
qinxuyang <1106207638@qq.com>
2022-07-06 21:47:05 +0800

    添加加载中的icon动态样式

--- src/views/hj-icon.vue
+++ src/views/hj-icon.vue
@@ -1,16 +1,27 @@
 <template>
-  <svg class="hj-icon"  aria-hidden="true">
+  <svg class="hj-icon" :class="name==='jiazai'?'loading':''" aria-hidden="true">
     <use :xlink:href="`#i-${name}`"></use>
   </svg>
 </template>
 <script lang="ts">
 export default {
-  props:['name']
+  props: ["name"],
 };
 </script>
 <style lang="less" scoped>
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 .hj-icon {
   width: 1.1em;
   height: 1.1em;
+}
+.loading{
+  animation: spin 1s infinite linear;
 }
 </style>
