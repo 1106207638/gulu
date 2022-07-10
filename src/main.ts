fee6f832f351e7f1f131354739755595ddb2303c
qinxuyang <1106207638@qq.com>
2022-07-10 21:41:09 +0800

    使用chai测试了button组件。click事件单元测试有bug，但是功能是正常的，所以暂时跳过。（代码暂未优化）

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
- import Buttom from '/src/views/button/hj_utton.vue'
- import Icon from '/src/views/hj-icon.vue'
+import Buttom from '/src/views/button/hj_utton.vue'
+import Icon from '/src/views/hj-icon.vue'
 import Hjbuttongroup from '/src/views/button/hj-button-group.vue'
 Vue.component('hj-button', Buttom)
 Vue.component('hj-icon', Icon)
@@ -16,3 +16,96 @@ new Vue({
 
   render: h => h(App)
 }).$mount('#app')
+
+
+import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
+const expect = chai.expect
+console.log(expect)
+{
+  const constructor = Vue.extend(Buttom)
+  console.log(constructor)
+  const button = new constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  button.$mount()
+  const useElement: any = button.$el.querySelector('use')
+  console.log(useElement)
+  const href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-settings')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const constructor = Vue.extend(Buttom)
+  console.log(constructor)
+  const button = new constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  button.$mount()
+  const useElement: any = button.$el.querySelector('use')
+  console.log(useElement)
+  const href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-jiazai')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const constructor = Vue.extend(Buttom)
+  console.log(constructor)
+  const button = new constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  button.$mount(div)
+  const svg: any = button.$el.querySelector('svg')
+  const roder = window.getComputedStyle(svg).order
+  expect(roder).to.eq('1')
+  button.$el.remove()
+  button.$destroy()
+
+}
+
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const constructor = Vue.extend(Buttom)
+  console.log(constructor)
+  const button = new constructor({
+    propsData: {
+      icon: 'settings',
+      iconPostition: 'right'
+    }
+  })
+  button.$mount(div)
+  const svg: any = button.$el.querySelector('svg')
+  const roder = window.getComputedStyle(svg).order
+  expect(roder).to.eq('2')
+  button.$el.remove()
+  button.$destroy()
+}
+//点击事件的单元测试有bug，但是功能是正常的
+// {
+//   const constructor = Vue.extend(Buttom)
+//   const gbutton = new constructor({
+//     propsData: {
+//       icon: 'settings',
+//     }
+//   }).$mount('#test')
+//   let spy = chai.spy(function () { })
+//   gbutton.$on('click', spy)
+//   let button: any = gbutton.$el
+//   button.click()
+//   expect(spy).to.have.been.called()
+//   console.log(button)
+// }
